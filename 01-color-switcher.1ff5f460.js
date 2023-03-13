7f9a7a9403dc1079d0619412e29b0b32ed846202
IhorPinchuk <115532409+IhorPinchuk@users.noreply.github.com>
2023-03-13 19:57:49 +0000

    Deploying to gh-pages from @ IhorPinchuk/goit-js-hw-09@281df4da8dff6156017698a2a73cd129d0f5490a 🚀

---
+++ 01-color-switcher.1ff5f460.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("body"),t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");console.log(o),console.log(t),console.log(e)}();
+//# sourceMappingURL=01-color-switcher.1ff5f460.js.map
