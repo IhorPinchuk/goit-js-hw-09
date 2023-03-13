281df4da8dff6156017698a2a73cd129d0f5490a
IhorPinchuk <pinchukkk@ukr.net>
2023-03-13 21:57:25 +0200

    (((

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+// const refs = {
+//   bodyEl: document.querySelector('body'),
+//   startBtn: document.querySelector('button[data-start]'),
+//   stopBtn: document.querySelector('button[data-stop]'),
+// };
+// console.log(refs.bodyEl);
+// console.log(refs.startBtn);
+// console.log(refs.stopBtn);
 
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+console.log(bodyEl);
+console.log(startBtn);
+console.log(stopBtn);
